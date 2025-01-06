b910b005049b13fae1f0e7377450341a2019bc19
shikazuki <shikazuki0@gmail.com>
2025-01-06 10:28:27 +0900

    create FSx OnTap.

--- main.tf
+++ main.tf
@@ -17,6 +17,7 @@ module "linux_instance" {
   root_volume_size = 8
 }
 
+# Use the below candidates.
 #      "Name": "/aws/service/ami-windows-latest/Windows_Server-2016-English-Full-Base"
 #      "Name": "/aws/service/ami-windows-latest/Windows_Server-2022-English-Full-Base"
 #      "Name": "/aws/service/ami-windows-latest/Windows_Server-2025-English-Full-Base"
@@ -33,27 +34,102 @@ module "windows_instance" {
   vpc_id           = module.network.vpc_id
   vpc_cidr         = module.network.vpc_cidr
   subnet_id        = module.network.pri_a_1_id
-  root_volume_size = 50
+  root_volume_size = 50 # minimum volume size
 }
 
+locals {
+  prefix = "fsx-netapp"
+  ingress_tcp = [
+    # [from_port, to_port]
+    [22, 22],
+    [111, 111],
+    [135, 135],
+    [139, 139],
+    [161, 162],
+    [443, 443],
+    [445, 445],
+    [635, 635],
+    [749, 749],
+    [2049, 2049],
+    [3260, 3260],
+    [4045, 4046],
+    [10000, 10000],
+    [11104, 11105],
+  ]
+  ingress_udp = [
+    # [from_port, to_port]
+    [111, 111],
+    [135, 135],
+    [137, 137],
+    [139, 139],
+    [161, 162],
+    [635, 635],
+    [2049, 2049],
+    [4045, 4046],
+    [4049, 4049],
+  ]
+}
+
+resource "aws_security_group" "main" {
+  name   = "${local.prefix}-sg"
+  vpc_id = module.network.vpc_id
+
+
+  ingress {
+    from_port   = -1
+    to_port     = -1
+    protocol    = "icmp"
+    cidr_blocks = [module.network.vpc_cidr]
+  }
+
+  dynamic "ingress" {
+    for_each = local.ingress_tcp
+    content {
+      from_port   = ingress.value[0]
+      to_port     = ingress.value[1]
+      protocol    = "tcp"
+      cidr_blocks = [module.network.vpc_cidr]
+    }
+  }
+
+
+  dynamic "ingress" {
+    for_each = local.ingress_udp
+    content {
+      from_port   = ingress.value[0]
+      to_port     = ingress.value[1]
+      protocol    = "udp"
+      cidr_blocks = [module.network.vpc_cidr]
+    }
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+    description = "All traffic is allowed"
+  }
+}
 
 resource "aws_fsx_ontap_file_system" "main" {
   storage_capacity                = 1024
   subnet_ids                      = [module.network.pri_a_1_id]
+  security_group_ids              = [aws_security_group.main.id]
   deployment_type                 = "SINGLE_AZ_1"
   ha_pairs                        = 1
   throughput_capacity_per_ha_pair = 128
   preferred_subnet_id             = module.network.pri_a_1_id
 
   tags = {
-    Name = "fsx-netapp"
+    Name = "${local.prefix}-fs"
   }
 }
 
 resource "aws_fsx_ontap_storage_virtual_machine" "svm" {
-  file_system_id = aws_fsx_ontap_file_system.main.id
-  name           = "fsx-netapp-svm"
-  svm_admin_password = "Password@Difficult" # CAUTION: NOT SECURE
+  file_system_id     = aws_fsx_ontap_file_system.main.id
+  name               = "${local.prefix}-svm"
+  svm_admin_password = "Password@1234" # CAUTION: NOT SECURE
 }
 
 resource "aws_fsx_ontap_volume" "volume" {
@@ -62,4 +138,5 @@ resource "aws_fsx_ontap_volume" "volume" {
   size_in_megabytes          = 1024
   storage_efficiency_enabled = true
   storage_virtual_machine_id = aws_fsx_ontap_storage_virtual_machine.svm.id
+  security_style             = "NTFS"
 }
--- modules/ec2-instance/main.tf
+++ modules/ec2-instance/main.tf
@@ -22,66 +22,15 @@ resource "aws_iam_instance_profile" "main" {
   role = aws_iam_role.main.name
 }
 
-locals {
-  ingress_tcp = [
-    # [from_port, to_port]
-    [22, 22],
-    [111, 111],
-    [135, 135],
-    [139, 139],
-    [161, 162],
-    [443, 443],
-    [635, 635],
-    [749, 749],
-    [2049, 2049],
-    [3260, 3260],
-    [4045, 4046],
-    [10000, 10000],
-    [11104, 11105],
-  ]
-  ingress_udp = [
-    # [from_port, to_port]
-    [111, 111],
-    [135, 135],
-    [139, 139],
-    [635, 635],
-    [2049, 2049],
-    [4045, 4046],
-    [4049, 4049],
-  ]
-}
-
 resource "aws_security_group" "main" {
   name   = "${var.name}-sg"
   vpc_id = var.vpc_id
 
-
   ingress {
-    from_port   = -1
-    to_port     = -1
-    protocol    = "icmp"
-    cidr_blocks = [var.vpc_cidr]
-  }
-
-  dynamic "ingress" {
-    for_each = local.ingress_tcp
-    content {
-      from_port   = ingress.value[0]
-      to_port     = ingress.value[1]
-      protocol    = "tcp"
-      cidr_blocks = [var.vpc_cidr]
-    }
-  }
-
-
-  dynamic "ingress" {
-    for_each = local.ingress_udp
-    content {
-      from_port   = ingress.value[0]
-      to_port     = ingress.value[1]
-      protocol    = "udp"
-      cidr_blocks = [var.vpc_cidr]
-    }
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
   }
 
   egress {
@@ -93,9 +42,30 @@ resource "aws_security_group" "main" {
   }
 }
 
+# To connect by using Windows Fleet Manager
+resource "tls_private_key" "main" {
+  algorithm = "RSA"
+  rsa_bits  = 4096
+}
+resource "aws_key_pair" "main" {
+  key_name   = var.name
+  public_key = tls_private_key.main.public_key_openssh
+}
+resource "aws_ssm_parameter" "keypair_pem" {
+  name  = "/${var.name}/keypair.pem"
+  value = tls_private_key.main.private_key_pem
+  type  = "SecureString"
+}
+resource "aws_ssm_parameter" "keypair_pub" {
+  name  = "/${var.name}/keypair.pub"
+  value = tls_private_key.main.public_key_openssh
+  type  = "SecureString"
+}
+
 resource "aws_instance" "main" {
   ami                  = var.ami
   instance_type        = var.instance_type
+  key_name             = aws_key_pair.main.key_name
   subnet_id            = var.subnet_id
   iam_instance_profile = aws_iam_instance_profile.main.name
   vpc_security_group_ids = [
